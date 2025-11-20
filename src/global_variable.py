f2e918e96349861474b0e01f47536931b24f95cd
开心-开心急了 <jxz911911@qq.com>
2025-11-20 11:37:51 +0800

    add: global clipboard object

--- src/global_variable.py
+++ src/global_variable.py
@@ -1,5 +1,10 @@
 from pathlib import Path
+from PySide6.QtGui import QGuiApplication
+
 STAR = "*"
 DEFAULT_NAME = "无标题"
 BASE_NAME = " - 记事本"
 HOME_PATH = str(Path.home())
+
+
+GLOBAL_CLIPBOARD = QGuiApplication.clipboard 
