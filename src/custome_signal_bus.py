950f68b5af3619686a92c5a19a0c118d04a7c5b5
开心-开心急了 <jxz911911@qq.com>
2025-11-21 12:36:43 +0800

    add: cursor postion signal

--- src/custome_signal_bus.py
+++ src/custome_signal_bus.py
@@ -5,6 +5,7 @@ class SignalBus(QObject):
     statusbar_action_checked = Signal(bool) # 状态栏勾选行为
     line_warp_action_checked = Signal(bool) # 换行勾选行为
     has_text = Signal(bool) # 是否有文本
+    find_cursor_postion_signal = Signal(str) # 查找光标位置 位于顶端或底部
 
 # 创建全局信号总线实例（只实例化一次）
 signal_bus = SignalBus()
